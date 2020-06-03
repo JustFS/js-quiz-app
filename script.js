c6912a251f2530b648a73c834bc46d9eeed11362
Julien Az <54361213+JustFS@users.noreply.github.com>
2020-06-03 18:51:42 +0200

    added quiz object

--- script.js
+++ script.js
@@ -1,3 +1,20 @@
+class Question {
+  constructor(text, choices, answer) {
+    this.text = text;
+    this.choices = choices;
+    this.answer = answer;
+  }
+  isCorrectAnswer(choice) {
+    return this.answer === choice;
+  }
+}
+let questions = [
+  new Question("Who was the first President of the United States?", [ "George Washington", "Thomas Jefferson", "Thomas Edison", "I don't know" ], "George Washington"),
+  new Question("What is the answer to the Ultimate Question of Life, the Universe, and Everything?", ["Pi","42", "Wah?", "I don't know"], "42"),
+  new Question("Do you love to code?", ["No","Yes", "Hell Yeah", "No"], "Hell Yeah"),
+  new Question("What's the best programming language?", ["Javascript","C#", "Php", "Python"], "Javascript"),
+];
+
 class Quiz {
   constructor(questions) {
     this.score = 0;
@@ -18,73 +35,52 @@ class Quiz {
   }
 }
 
-const QuizUI = {
-  displayNext: function () {
-      if (quiz.hasEnded()) {
-        this.displayScore();
-      } else {
-        this.displayQuestion();
-        this.displayChoices();
-        this.displayProgress();
-      }
-  },
 
-  displayQuestion: function() {
-      this.populateIdWithHTML("question", quiz.getCurrentQuestion().text);
+const Display = {
+  populateSelector: function(id, text) {
+    let element = document.getElementById(id);
+    element.innerHTML = text;
   },
-  displayChoices: function() {
-      var choices = quiz.getCurrentQuestion().choices;
+  question: function() {
+    this.populateSelector("question", quiz.getCurrentQuestion().text);
+  },
+  choices: function() {
+    var choices = quiz.getCurrentQuestion().choices;
 
-      for(var i = 0; i < choices.length; i++) {
-          this.populateIdWithHTML("choice" + i, choices[i]);
-          this.guessHandler("guess" + i, choices[i]);
-      }
-  },
-  displayScore: function() {
-      var gameOverHTML = "<h1>Game Over</h1>";
-      gameOverHTML += "<h2> Your score is: " + quiz.score + "</h2>";
-      this.populateIdWithHTML("quiz", gameOverHTML);
-  },
-  
-  populateIdWithHTML: function(id, text) {
-      var element = document.getElementById(id);
-      element.innerHTML = text;
+    for(let i = 0; i < choices.length; i++) {
+      this.populateSelector("choice" + i, choices[i]);
+      this.guessHandler("guess" + i, choices[i]);
+    }
   },
   guessHandler: function(id, guess) {
-      var button = document.getElementById(id);
-      button.onclick = function() {
-          quiz.guess(guess);
-          QuizUI.displayNext();
-      }
+    let button = document.getElementById(id);
+    button.onclick = function() {
+      quiz.guess(guess);
+      quizApp();
+    }
   },
-  
-  displayProgress: function() {
-      var currentQuestionNumber = quiz.currentQuestionIndex + 1;
-      this.populateIdWithHTML("progress", "Question " + currentQuestionNumber + " of " + quiz.questions.length);
-  }
+  progress: function() {
+    let currentQuestionNumber = quiz.currentQuestionIndex + 1;
+    this.populateSelector("progress", "Question " + currentQuestionNumber + " of " + quiz.questions.length);
+  },
+  endQuiz: function() {
+    endQuizHTML = `
+      <h1>Quiz terminé !</h1>
+      <h2> votre score est de : ${quiz.score} / ${quiz.questions.length}`;
+      this.populateSelector("quiz", endQuizHTML);
+  },
 };
 
-class Question {
-  constructor(text, choices, answer) {
-    this.text = text;
-    this.choices = choices;
-    this.answer = answer;
-  }
-  isCorrectAnswer(choice) {
-    return this.answer === choice;
+quizApp = () => {
+  if (quiz.hasEnded()) {
+    Display.endQuiz();
+  } else {
+    Display.question();
+    Display.choices();
+    Display.progress();
   }
 }
-let questions = [
-  new Question("Who was the first President of the United States?", [ "George Washington", "Thomas Jefferson", "Thomas Edison", "I don't know" ], "George Washington"),
-  new Question("What is the answer to the Ultimate Question of Life, the Universe, and Everything?", ["Pi","42", "Wah?", "I don't know"], "42"),
-  new Question("Do you love to code?", ["No","Yes", "Hell Yeah", "No"], "Hell Yeah"),
-  new Question("What's the best programming language?", ["Javascript","C#", "Php", "Python"], "Javascript"),
-];
 
 //Create Quiz
 let quiz = new Quiz(questions);
-
-//Display Quiz
-QuizUI.displayNext();
-
-
+quizApp();
